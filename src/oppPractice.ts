ab80022239bc9483c6e23310279a85cd62ca492c
Andrey-Shtanko <shtankoandrew90@gmail.com>
2022-12-24 23:57:08 +0200

    add-correct-answer

--- src/oppPractice.ts
+++ src/oppPractice.ts
@@ -23,7 +23,7 @@ class Person {
 abstract class House  { 
     door: 'open' | 'close';
     key: Key;
-    tenants: object[] = [];
+    tenants: Person[] = [];
     constructor(key: Key, door:'close') { 
         this.key = key
         this.door = door;
@@ -55,3 +55,64 @@ myHouse.comeIn(owner);
 console.log(myHouse.tenants);
 console.log(myHouse.key);
 
+/**
+ * ///////////////////////////////////КОД ВЫШЕ РАБЛОТАЕТ, НО  /////////////////////////
+ *  ////////////////////////////////НАДО БЫЛО СДЕЛАТЬ ТАК://///////////////////////////////////
+ * 
+ * 
+
+ * class Key {
+  private signature: number;
+
+  constructor () {
+    this.signature = Math.random();
+  }
+
+  getSignature (): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor (private key:Key) {}
+  getKey (): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door = false;
+  private tenants: Person[] = [];
+  constructor (protected key:Key) {}
+
+  comeIn (person: Person):void {
+    if (!this.door) {
+      throw new Error('Door is close');
+    }
+
+    this.tenants.push(person);
+    console.log('Person inside');
+  }
+
+  abstract openDoor (key:Key): boolean;
+}
+
+class MyHouse extends House {
+  openDoor (key:Key) {
+    if (key.getSignature() !== this.key.getSignature()) {
+      throw new Error('Key to another door');
+    }
+
+    return this.door = true;
+  }
+}
+
+const key = new Key();
+
+const house = new MyHouse(key);
+const person = new Person(key);
+
+house.openDoor(person.getKey());
+
+house.comeIn(person);
+ */
